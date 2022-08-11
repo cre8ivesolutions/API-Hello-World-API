241027eea5df4b9d5b0ed02e7914ddfe84520e4b
cre8ivesolutions <tammerz78@gmail.com>
2022-08-10 20:17:55 -0500

    done

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const mongoose = require('mongoose')
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
-mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true}, 
     () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
 )
 
@@ -17,6 +20,13 @@ app.use(express.urlencoded({extended: true}))
 app.get('/', (req, res) => {
   res.send('Welcome to the Hello World! API')
 })
+// Languages: 
+const languagesController = require('./controllers/language_controller')
+app.use('/languages', languagesController)
+
+// // Random Languages: 
+// // const languagesController = require('./controllers/language_controller')
+// app.use('/languages/random', languagesController)
 
 // LISTEN
 app.listen(PORT, () => {
